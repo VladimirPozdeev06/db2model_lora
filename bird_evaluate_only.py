857b805c3bd2e270dec57af36fb22541ecb0f671
Pozdeev_Vladimir <vova.pozdeev.06@mail.ru>
2026-07-16 21:22:19 +0300

    fix bird_evaluate_only script

--- bird_evaluate_only.py
+++ bird_evaluate_only.py
@@ -1,15 +1,16 @@
+import json
 import os
+import sys
 
 from benchmarks.evaluate_bird import run_evaluation, print_evaluation_report
 
-db_url = os.getenv("BENCHMARK_DB_URL")
+db_url = os.getenv("BENCHMARK_DB_URL", "localhost:5444")
+predictions_file = sys.argv[1] if len(sys.argv) > 1 else "./query_results.json"
+answer_file = sys.argv[2] if len(sys.argv) > 2 else "./data/bird_small.json"
 
-def _load_queries(self) -> List[dict]:
-    with open("./query_results", "r") as f:
-        return json.load(f)
+with open(predictions_file, "r", encoding="utf-8") as f:
+    predictions = json.load(f)
 
-predictions = _load_queries()
-
-report = run_evaluation(predictions, "./data/bird_small.json", db_url)
+report = run_evaluation(predictions, answer_file, db_url)
 
 print_evaluation_report(report)
